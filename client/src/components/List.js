ab828f477f8560d0abeac7fb56e7a3cec921fb78
신용주 <lukas.sin@kakaocorp.com>
2019-09-25 18:37:31 +0900

    Test normal toggle

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -5,11 +5,17 @@ import style from './List.scss'
 const List =  (props) => {  
   const [list ,setList] = useState([])
   const [toggle, setToggle] = useState(false)
+  const [nomalToggle, actionNormalToggle] = useState(false)
   const {tokenExpiredHandler} = useContext(AuthContext)
 
-  const onHandleToggle = (e) => {
+  const onGetListHandleToggle = (e) => {
     setListData()
   }
+
+  const onToggleHandler = (e) => {
+    actionNormalToggle(!nomalToggle)
+  }
+
 
   const setListData = () => {
 
@@ -23,7 +29,7 @@ const List =  (props) => {
     }).catch((error) => { console.log(error)
       tokenExpiredHandler()
     })
-    
+
   }
 
   const setListError = () => {
@@ -34,10 +40,35 @@ const List =  (props) => {
   return (<>
   <h2>Verify</h2>
   <div className="verify">
-    <button onClick={e=>onHandleToggle(e)}>Click me</button>
+    <button onClick={e=>onGetListHandleToggle(e)}>Click me</button>
     <ul>
       { toggle ? list : <></>}
     </ul>
+  </div>
+  <div>
+    <button onClick={e=>onToggleHandler(e)}>Click me</button>
+    { nomalToggle ?  <p>
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+      asldm;alsm;lasmcl;mas;lcmal;smcl;asmcl;asmcl;ams
+    </p> : <></>}
   </div>
   </>)
 }
